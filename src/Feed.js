70b5284238a25a1176ce8b0b5389f8efcde1de36
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-29 13:40:48 +0800

    Added and successfully integrated firebase, Finished feed post as well.

--- src/Feed.js
+++ src/Feed.js
@@ -1,31 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption';
+import Post from './Post';
 
 import PhotoIcon from '@material-ui/icons/Photo';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
+import { db } from './firebase';
+
+import firebase from 'firebase';
+
 function Feed() {
+  const [input, setInput] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          data: doc.data(),
+        }
+      )))
+    ))
+  }, [])
+
+  const sendPost = e => {
+    e.preventDefault();
+
+    db.collection('posts').add({
+      name: 'Karl Jose Buena',
+      description: 'this is a test',
+      message: input,
+      photoUrl: '',
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  };
+
   return (
     <div className='feed'>
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text"/>
-            <button type="submit">Send</button>
+            <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
-          <InputOption Icon={PhotoIcon} title='Photo' color='#70BFF9'/>
-          <InputOption Icon={YouTubeIcon} title='Video' color='#E7A33E'/>
-          <InputOption Icon={EventAvailableIcon} title='Event' color='#C0CBCD'/>
-          <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7FC15E'/>
+          <InputOption Icon={PhotoIcon} title='Photo' color='#70BFF9' />
+          <InputOption Icon={YouTubeIcon} title='Video' color='#E7A33E' />
+          <InputOption Icon={EventAvailableIcon} title='Event' color='#C0CBCD' />
+          <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7FC15E' />
         </div>
       </div>
+
+      {/* Posts */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   )
 }
